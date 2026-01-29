c7e195301f37457bf106ae5da49df1effdb3432f
Markian966 <markian160505@gmail.com>
2026-01-29 13:32:43 +0100

    configured db connection and models ctruct

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  logging: false,
+});
+
+module.exports = sequelize;
